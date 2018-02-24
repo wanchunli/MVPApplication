6e5ca352abdb06843fcb5e53f95ccafafe1cdb0d
wanchun <1596900283@qq.com>
2018-02-24 22:11:46 +0800

    添加指南针

--- app/src/main/java/com/wan/grace/mvpapplication/ui/fragment/platform/PlatformFragment.java
+++ app/src/main/java/com/wan/grace/mvpapplication/ui/fragment/platform/PlatformFragment.java
@@ -16,7 +16,6 @@ import com.wan.grace.mvpapplication.ui.fragment.banner.BannerViewHolder;
 import com.zhouwei.mzbanner.MZBannerView;
 import com.zhouwei.mzbanner.holder.MZHolderCreator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -128,7 +127,7 @@ public class PlatformFragment extends MVPBaseFragment<PlatFormView, PlatFormPres
     @Override
     public void onResume() {
         super.onResume();
-//        mMZBanner.start();
+        mMZBanner.start();
     }
 
     private void setBanner(List<Movie> movies){
