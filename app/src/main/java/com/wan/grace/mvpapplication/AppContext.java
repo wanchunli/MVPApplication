33df65a53d3e89a879223be5c58e4b5a1b2278aa
wanchun <1596900283@qq.com>
2018-02-28 17:22:22 +0800

    增加二维码从相册获取扫描功能

--- app/src/main/java/com/wan/grace/mvpapplication/AppContext.java
+++ app/src/main/java/com/wan/grace/mvpapplication/AppContext.java
@@ -1,5 +1,6 @@
 package com.wan.grace.mvpapplication;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.content.IntentFilter;
@@ -7,7 +8,13 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.lqr.imagepicker.ImagePicker;
+import com.lqr.imagepicker.loader.ImageLoader;
+import com.lqr.imagepicker.view.CropImageView;
 import com.wan.grace.mvpapplication.bean.Movie;
 import com.wan.grace.mvpapplication.bean.MovieSubject;
 import com.wan.grace.mvpapplication.bean.User;
@@ -29,10 +36,40 @@ public class AppContext extends Application {
         super.onCreate();
         sInstance = this;
         mAppShared = new AppShared(this);
+        initImagePicker();
     }
 
     public static AppContext getInstance() {
         return sInstance;
+    }
+
+    /**
+     * 初始化仿微信控件ImagePicker
+     */
+    private void initImagePicker() {
+        ImagePicker imagePicker = ImagePicker.getInstance();
+        imagePicker.setImageLoader(new ImageLoader() {
+            @Override
+            public void displayImage(Activity activity, String path, ImageView imageView, int width, int height) {
+                Glide.with(getInstance()).load(Uri.parse("file://" + path).toString()).centerCrop().into(imageView);
+            }
+
+            @Override
+            public void clearMemoryCache() {
+
+            }
+        });
+        //设置图片加载器
+        imagePicker.setMultiMode(false);
+        imagePicker.setShowCamera(true);  //显示拍照按钮
+        imagePicker.setCrop(true);        //允许裁剪（单选才有效）
+        imagePicker.setSaveRectangle(true); //是否按矩形区域保存
+        imagePicker.setSelectLimit(9);    //选中数量限制
+        imagePicker.setStyle(CropImageView.Style.RECTANGLE);  //裁剪框的形状
+        imagePicker.setFocusWidth(800);   //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setFocusHeight(800);  //裁剪框的高度。单位像素（圆形自动取宽高最小值）
+        imagePicker.setOutPutX(1000);//保存文件的宽度。单位像素
+        imagePicker.setOutPutY(1000);//保存文件的高度。单位像素
     }
 
     @Override
--- app/src/main/java/com/wan/grace/mvpapplication/ui/ScanActivity.java
+++ app/src/main/java/com/wan/grace/mvpapplication/ui/ScanActivity.java
@@ -1,33 +1,54 @@
 package com.wan.grace.mvpapplication.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.lqr.imagepicker.ImagePicker;
+import com.lqr.imagepicker.bean.ImageItem;
+import com.lqr.imagepicker.ui.ImageGridActivity;
 import com.wan.grace.mvpapplication.R;
 import com.wan.grace.mvpapplication.base.MVPBaseActivity;
+import com.wan.grace.mvpapplication.thread.ThreadPoolFactory;
 import com.wan.grace.mvpapplication.ui.presenter.ScanPresenter;
 import com.wan.grace.mvpapplication.ui.view.ScanView;
+import com.wan.grace.mvpapplication.utils.PopupWindowUtils;
+import com.wan.grace.mvpapplication.utils.UIUtils;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zxing.QRCodeDecoder;
 import cn.bingoogolapple.qrcode.zxing.ZXingView;
 
 public class ScanActivity extends MVPBaseActivity<ScanView, ScanPresenter> implements ScanView,
         QRCodeView.Delegate, View.OnClickListener {
 
+    public static final int IMAGE_PICKER = 100;
     @BindView(R.id.zxingview)
     ZXingView mZxingview;
     @BindView(R.id.light_control)
     RelativeLayout lightControlLayout;
     @BindView(R.id.light_image)
     ImageView lightImageView;
+    @BindView(R.id.back_layout)
+    RelativeLayout backLayout;
     @BindView(R.id.more_layout)
     RelativeLayout moreLayout;
+    private FrameLayout mView;
+    private PopupWindow mPopupWindow;
 
     @Override
     protected int provideContentViewId() {
@@ -71,9 +92,39 @@ public class ScanActivity extends MVPBaseActivity<ScanView, ScanPresenter> imple
                 }
                 break;
             case R.id.more_layout:
-
+                showPopupMenu();
+                break;
+            case R.id.back_layout:
+                finish();
                 break;
         }
+    }
+
+    private void showPopupMenu() {
+        if (mView == null) {
+            mView = new FrameLayout(this);
+            mView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            mView.setBackgroundColor(UIUtils.getColor(R.color.white));
+
+            TextView tv = new TextView(this);
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, UIUtils.dip2Px(45));
+            tv.setLayoutParams(params);
+            tv.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+            tv.setPadding(UIUtils.dip2Px(20), 0, 0, 0);
+            tv.setTextColor(UIUtils.getColor(R.color.black));
+            tv.setTextSize(14);
+            tv.setText(UIUtils.getString(R.string.select_qr_code_from_ablum));
+            mView.addView(tv);
+
+            tv.setOnClickListener(v -> {
+                mPopupWindow.dismiss();
+                Intent intent = new Intent(ScanActivity.this, ImageGridActivity.class);
+                startActivityForResult(intent, IMAGE_PICKER);
+            });
+        }
+        mPopupWindow = PopupWindowUtils.getPopupWindowAtLocation(mView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, getWindow().getDecorView().getRootView(), Gravity.BOTTOM, 0, 0);
+        mPopupWindow.setOnDismissListener(() -> PopupWindowUtils.makeWindowLight(ScanActivity.this));
+        PopupWindowUtils.makeWindowDark(this);
     }
 
     @Override
@@ -118,5 +169,29 @@ public class ScanActivity extends MVPBaseActivity<ScanView, ScanPresenter> imple
     private void vibrate() {
         Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(200);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == ImagePicker.RESULT_CODE_ITEMS) {//返回多张照片
+            if (data != null) {
+                final ArrayList<ImageItem> images = (ArrayList<ImageItem>) data.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
+                if (images != null && images.size() > 0) {
+                    //取第一张照片
+                    ThreadPoolFactory.getNormalPool().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            String result = QRCodeDecoder.syncDecodeQRCode(images.get(0).path);
+                            if (TextUtils.isEmpty(result)) {
+                                UIUtils.showToast(UIUtils.getString(R.string.scan_fail));
+                            } else {
+                                handleResult(result);
+                            }
+                        }
+                    });
+                }
+            }
+        }
     }
 }
